4c178e5e2b13fadc7fba0bfa36549bec457fb2e8
Manish Kudtarkar <140542317+ManishKudtarkar@users.noreply.github.com>
2024-12-19 08:25:49 +0530

    Create name.py

---
+++ name.py
@@ -0,0 +1,15 @@
+data = ("Manish","Patel",567.89)
+print("Hello Mr. %s %s your balance is %.2f" %data)
+
+'{2},{1},{0}' .format(*'abc')
+
+'{0}{1}{0}' .format('ello','hihi')
+
+a=12
+b=30
+print(f'if a = {a} and b={b} then the sum will be {a+b}')
+
+def print_full_name(first, last):
+    # Write your code here
+    print(f"Hello {first} {last}! You just delved into python.")
+    
