daa3e71fa03f1c5324584fffa702ea31e6cb9093
Manish Kudtarkar <140542317+ManishKudtarkar@users.noreply.github.com>
2024-12-19 09:00:20 +0530

    Update mutation.py

--- mutation.py
+++ mutation.py
@@ -1,3 +1,9 @@
 def mutate_string(string, position, character):
     return string[:position] +character + string[position+1:]
     print (string)
+
+def mutate_string(string, position, character):
+    l = list(string)
+    l[position]= character
+    string = ''.join(l)
+    return string
